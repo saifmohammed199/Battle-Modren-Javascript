2d9588bc1bf0132c38558245beafc94159eb5420
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-03-21 14:15:47 +0530

    this code is updated using modren js

---
+++ Components/Loading.js
@@ -0,0 +1,51 @@
+import React from'react';
+import PropTypes from'prop-types';
+
+const style={
+    content:{
+        textAlign:'center',
+        fontSize:'35px'
+    }
+};
+
+class Loading extends React.Component{
+ 
+    static PropTypes={
+        text:PropTypes.string.isRequired,
+        speed:PropTypes.number.isRequired,
+    }
+    
+    static defaultProps={
+        text:'Loading',
+        speed:300
+    };
+    state ={
+        text:this.props.text
+    }
+    
+       componentDidMount(){
+
+        const{text, speed}=this.props
+        const stopper= text +'...';
+
+        this.interval = window.setInterval(()=>{
+            this.state.text===stopper
+            ?this.setState(()=>({text:this.props.text}))
+            : this.setState((prevState)=>({text:prevState.text+'.'}))
+        },speed)
+    }
+    componentWillUnmount(){
+        window.clearInterval(this.interval);
+    }
+    render(){
+        return(
+            <p style={style.content}>
+                {this.state.text}
+            </p>
+        )
+    }
+}
+
+
+
+export default Loading;
